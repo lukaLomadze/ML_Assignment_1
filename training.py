8387cd5c9f67c467b6034752c781540990c9a1f6
lukaLomadze <lloma23@freeuni.edu.ge>
2026-04-13 23:59:10 +0400

    finishing

--- training.py
+++ training.py
@@ -3,6 +3,9 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
 import numpy as np
 
+def rmsle(y_true, y_pred):
+    return np.sqrt(mean_squared_error(np.log1p(y_true), np.log1p(y_pred)))
+
 
 def train_model(x,y,x_val,y_val, name,model, scoring):
     with mlflow.start_run(run_name=name) :
@@ -10,23 +13,30 @@ def train_model(x,y,x_val,y_val, name,model, scoring):
         cv_rmsle_mean = -cv_scores.mean()
         cv_rmsle_std = cv_scores.std()
         model.fit(x, y)
-        t_rmsle= np.sqrt(mean_squared_error(y, model.predict(x)))
-        v_rmsle = np.sqrt(mean_squared_error(y_val, model.predict(x_val)))
+
+        t_pred= model.predict(x)
+        v_pred= model.predict(x_val)
+
+
+        t_rmsle= rmsle(y,t_pred)
+        v_rmsle = rmsle(y_val,v_pred)
+
         params ={"name": type(model).__name__}
         params.update(model.get_params())
         mlflow.log_params(params)
+
         mlflow.log_metrics({
             "cv_rmsle_mean": cv_rmsle_mean,
             "cv_rmsle_std": cv_rmsle_std,
             "train_rmsle": t_rmsle,
-            "validation_rmsle": v_rmsle,
+            "val_rmsle": v_rmsle,
             "overfit": v_rmsle-t_rmsle
 
 
 
         })
 
-        mlflow.sklearn.log_model(model, name ="model")
+        mlflow.sklearn.log_model(model, "model")
         print("name : ", name)
         print("cv_rmsle_mean: ", cv_rmsle_mean)
         print("cv_rmsle_std: ", cv_rmsle_std)
